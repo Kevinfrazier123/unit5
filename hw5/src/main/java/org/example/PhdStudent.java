1279f0a6c469317c40234d05e6703d0d2a628965
kevin <kevin.frazier059@gmail.com>
2024-03-15 04:09:12 -0400

    Phd student complete

--- hw5/src/main/java/org/example/PhdStudent.java
+++ hw5/src/main/java/org/example/PhdStudent.java
@@ -1,7 +1,14 @@
 package org.example;
 
 class PhdStudent extends GraduateStudent {
-// . . .
+
+    // Private fields
+
+private String name;
+private String id;
+private String advisor;
+private String researchSubject;
+private int crn;
 
 
     public PhdStudent (String name, String id , String advisor, String researchSubject , int crn ) {
@@ -9,12 +16,51 @@ class PhdStudent extends GraduateStudent {
 
         super ( name , id , crn );
         // . . .
+        this.name = name;
+        this.id = id;
+        this.advisor = advisor;
+        this.researchSubject = researchSubject;
+        this.crn = crn;
 
     }
 
     @Override
     public void printInvoice() {
-        // . . .
+
+        double research = 700;
+        double health = 35;
+        double total = research + health;
+        
+
+
+        System.out.println("\nVALENCE COLLEGE");
+        System.out.println("ORLANDO FL 10101");
+        System.out.println("-----------------------------------");
+
+        System.out.println("Fee Invoice Prepared for Student: " + "\n"+
+                id + " - " + name);
+        System.out.println("\n RESEARCH                 $" + research + "");
+        System.out.println(researchSubject + "\n");
+        System.out.println("         Health & id fees " + "$" + health );
+        System.out.println("-----------------------------");
+        System.out.println("Total Payments      $" + total);
+
+
+
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
